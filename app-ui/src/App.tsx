1cc6e8722ef81159e3f911727a826441bf1cdbf2
bhavyapk <bhavyapkrishna@gmail.com>
2023-12-04 20:48:20 -0500

    fixed bugs in frontend

--- app-ui/src/App.tsx
+++ app-ui/src/App.tsx
@@ -14,146 +14,146 @@ type Note = {
 function App() {
   const [notes, setNotes] = useState<Note[]>([]);
 
-    //add a note
-    const [title, setTitle] = useState("");
-    const [content, setContent] = useState("");
-    const [selectedNote, setSelectedNote] = useState<Note | null>(null);
+  //add a note
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [selectedNote, setSelectedNote] = useState<Note | null>(null);
 
-    useEffect(() => {
-      const fetchNotes = async () => {
-        try {
-          const response = await fetch("https://bpk31-notes-server.onrender.com/api/notes");
-          const notes: Note[] = await response.json();
-          setNotes(notes);
-        } catch (error) {
-          console.log(error);
-        }
-      };
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const response = await fetch("https://bpk31-notes-server.onrender.com/api/notes");
+        const notes: Note[] = await response.json();
+        setNotes(notes);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-      fetchNotes();
-    }, []);
+    fetchNotes();
+  }, []);
 
-    const handleNoteClick = (note:Note) => {
-      setSelectedNote(note);
-      setTitle(note.title);
-      setContent(note.content);
+  const handleNoteClick = (note: Note) => {
+    setSelectedNote(note);
+    setTitle(note.title);
+    setContent(note.content);
+  }
+
+  const handleAddNote = async (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+      const response = await fetch("https://bpk31-notes-server.onrender.com/api/notes", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+          title,
+          content
+        })
+      });
+
+      const newNote = await response.json();
+
+      setNotes([newNote, ...notes]);
+      setTitle("");
+      setContent("");
+
+    } catch (error) {
+      console.log(error);
     }
 
-    const handleAddNote = async (event: React.FormEvent) => {
-      event.preventDefault();
-      try {
-        const response = await fetch("https://bpk31-notes-server.onrender.com/api/notes", {
-          method: "POST",
-          headers: {
-            "Content-type": "application/json"
-          },
-          body: JSON.stringify({
-            title,
-            content
-          })
-        });
 
-        const newNote = await response.json();
+  };
 
-        setNotes([newNote, ...notes]);
-        setTitle("");
-        setContent("");
+  const handleUpdateNote = async (event: React.FormEvent) => {
+    event.preventDefault();
 
-      } catch (error) {
-        console.log(error);
-      }
+    if (!selectedNote) {
+      return;
+    }
 
-      
-    };
+    try {
 
-    const handleUpdateNote = async (event: React.FormEvent) => {
-      event.preventDefault();
+      const response = await fetch(`https://bpk31-notes-server.onrender.com/api/notes/${selectedNote.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+          title,
+          content
+        })
+      });
 
-      if(!selectedNote) {
-        return;
-      }
+      const updatedNote = await response.json();
 
-      try {
-
-        const response = await fetch(`https://bpk31-notes-server.onrender.com/api/notes/${selectedNote.id}`, {
-          method: "PUT",
-          headers: {
-            "Content-type": "application/json"
-          },
-          body: JSON.stringify({
-            title,
-            content
-          })
-        });
-
-        const updatedNote = await response.json();
-
-        const updatedNotesList = notes.map((note) => 
+      const updatedNotesList = notes.map((note) =>
         note.id === selectedNote.id
-        ? updatedNote
-        : note
-        )
+          ? updatedNote
+          : note
+      )
 
-        setNotes(updatedNotesList);
-        setTitle("");
-        setContent("");
-        setSelectedNote(null);
-
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    const handleCancel = () => {
+      setNotes(updatedNotesList);
       setTitle("");
       setContent("");
       setSelectedNote(null);
+
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const handleDeleteNote = async (event: React.MouseEvent, noteId: number) => {
-      event.stopPropagation();
+  const handleCancel = () => {
+    setTitle("");
+    setContent("");
+    setSelectedNote(null);
+  }
 
-      try {
-        const response = await fetch(`https://bpk31-notes-server.onrender.com/api/notes/${noteId}`, {
-          method: "DELETE"
-        });
+  const handleDeleteNote = async (event: React.MouseEvent, noteId: number) => {
+    event.stopPropagation();
 
-        const updatedNotes = notes.filter(
-          (note) => note.id != noteId
-        );
-  
-        setNotes(updatedNotes);
+    try {
+      const response = await fetch(`https://bpk31-notes-server.onrender.com/api/notes/${noteId}`, {
+        method: "DELETE"
+      });
 
-      } catch (error) {
-        console.log(error);
-      }
-    };
+      const updatedNotes = notes.filter(
+        (note) => note.id != noteId
+      );
+
+      setNotes(updatedNotes);
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
       <div className="header">
-      <Box sx={{ flexGrow: 1, padding: 2}}>
-        <AppBar position="static" sx={{borderRadius: 2}}>
-          <Toolbar sx={{ justifyContent: 'center', position: 'relative', background: '#77e5a2', borderRadius: 2}}>
-            <Typography sx={{ fontWeight: '600' }} variant="h6" component="div" color="black">
-              Notekeeper
-            </Typography>{' '}
-          </Toolbar>
-        </AppBar>
-      </Box>
+        <Box sx={{ flexGrow: 1, padding: 2 }}>
+          <AppBar position="static" sx={{ borderRadius: 2 }}>
+            <Toolbar sx={{ justifyContent: 'center', position: 'relative', background: '#77e5a2', borderRadius: 2 }}>
+              <Typography sx={{ fontWeight: '600' }} variant="h6" component="div" color="black">
+                Notekeeper
+              </Typography>{' '}
+            </Toolbar>
+          </AppBar>
+        </Box>
       </div>
       <div>
-      <Box component="form" className = "note-form"
-        onSubmit={(event) => 
-        selectedNote
-        ? handleUpdateNote(event)
-        : handleAddNote(event)}>
-        <input value={title} 
-          onChange={(event) => setTitle(event.target.value)} 
-          placeholder="Title" required />
-        <textarea className="content-area" value={content}
-          onChange={(event) => setContent(event.target.value)}
-          placeholder="Enter Content" rows={5} required />
+        <Box component="form" className="note-form"
+          onSubmit={(event) =>
+            selectedNote
+              ? handleUpdateNote(event)
+              : handleAddNote(event)}>
+          <input value={title}
+            onChange={(event) => setTitle(event.target.value)}
+            placeholder="Title" required />
+          <textarea className="content-area" value={content}
+            onChange={(event) => setContent(event.target.value)}
+            placeholder="Enter Content" rows={5} required />
           {selectedNote ? (
             <div className="edit-buttons">
               <button type="submit">Save</button>
@@ -162,28 +162,33 @@ function App() {
           ) : (
             <button type="submit">Add Note</button>
           )}
-      </Box>
+        </Box>
       </div>
-    <div className="app-container">
-    <div>
-        <Typography />
-      </div>
-      <Box>
-      <div className="notes-grid">
-      {notes.map((note) => (
-        <div className="note-item"
-          onClick={() => handleNoteClick(note)}>
-          <div className="notes-header">
-            <button
-              onClick={(event) => handleDeleteNote(event, note.id )}>x</button>
-          </div>
-          <h2>{note.title}</h2>
-          <p>{note.content}</p>
+      <div className="app-container">
+        <div>
+          <Typography />
         </div>
-      ))}
-    </div>
-      </Box>
-    </div>
+        <Box>
+          <div className="notes-grid">
+            {notes.map((note) => (
+              <div className="note-item"
+                onClick={() => handleNoteClick(note)}>
+                <div className="notes-header">
+                  <button
+                    onClick={(event) => handleDeleteNote(event, note.id)}>x</button>
+                </div>
+                <h2>{note.title}</h2>
+                <p>{note.content.split('\n').map((line, index) => (
+                  <React.Fragment key={index}>
+                    {line}
+                    <br />
+                  </React.Fragment>
+                ))}</p>
+              </div>
+            ))}
+          </div>
+        </Box>
+      </div>
     </div>
   );
 }
